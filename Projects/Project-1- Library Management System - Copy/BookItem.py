fdbe236f3437099796cd4953f7f9e02b53e05663
kaulashish <kaulashish@protonmail.com>
2020-12-06 01:46:28 +0530

    Removed draw.io save file and copy(old code) of project.

--- Projects/Project-1- Library Management System - Copy/BookItem.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-class BookItem:
-    def __init__(self, book, isbn, rack):
-        self.book = book
-        self.isbn = isbn
-        self.rack = rack
-
-    def __repr__(self):
-        return str(self.isbn)
